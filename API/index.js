9333f8c506c7ef27d93641d7616954e139885bdb
Henna Virnes <t9vihe00@students.oamk.fi>
2020-11-06 11:27:08 +0200

    hiding the credentials

--- API/index.js
+++ API/index.js
@@ -1,23 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-//const db = require('./db');
-
-
-const { Client } = require('pg');
-
-const client = new Client({
-    user: 'wmewcoqahomuca',
-    host: 'ec2-54-75-150-32.eu-west-1.compute.amazonaws.com',
-    database: 'd6e4e41kp2e467',
-    password: 'd82fe89addeca7db91e01a8931cf9a07a11b67f521ee50e799cb9e8526f28ac6',
-    port: 5432,
-    ssl: { rejectUnauthorized: false }
-
-});
-
-
-client.connect();
+const client = require('./db');
 
 
 app.get('/', (req, res) => {
